6ff18b75c64f23a23179e9b2cfe75771d974bda0
Abdul Mustafa <117674574+AbdulMustfa@users.noreply.github.com>
2023-06-10 12:40:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const extendHex = (shortHex) => {
   // write your code here
+	const hex = shortHex.replace('#', '');
+
+  const r = hex[0];
+  const g = hex[1];
+  const b = hex[2];
+
+  // Create the full hex code by repeating each color component
+  const fullHex = `#${r}${r}${g}${g}${b}${b}`;
+
+  return fullHex;
 };
 
 // Do not change the code below.
